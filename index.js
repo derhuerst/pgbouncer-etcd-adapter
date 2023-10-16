6360126f558446c464f5bdfc322172288832cabd
Jannis R <mail@jannisr.de>
2023-10-16 23:20:34 +0300

    add debug-logging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import createDebug from 'debug'
 import {writeFile} from 'node:fs/promises'
 import {connectToEtcd} from './lib/etcd.js'
 import {
@@ -7,17 +8,36 @@ import {
 
 const LINUX_DEFAULT_CONFIG_BASE_DIR = '/etc/pgbouncer'
 
+const debug = createDebug('pgbouncer-etcd-adapter')
+
+const writeFileAndLog = async (file, data) => {
+	try {
+		await writeFile(file, data)
+		debug('successfully written', file)
+	} catch (err) {
+		debug('failed to write to', file, err)
+		throw err
+	}
+}
+
 const generatePgbouncerConfigFromEtc = async (opt = {}) => {
+	opt = {
+		...opt,
+	}
+	debug('options', opt)
+
 	const etcd = await connectToEtcd()
 
 	const generateConfig = async () => {
+		debug('regenerating config')
 		const etcdEntries = await etcd.getAll().strings()
+		debug('etcd entries', etcdEntries)
 		const pgbouncerIni = mapEtcdEntriesToPgbouncerIni(etcdEntries)
 		const userlistTxt = mapEtcdEntriesToUserlistTxt(etcdEntries)
 
 		await Promise.all([
-			writeFile(LINUX_DEFAULT_CONFIG_BASE_DIR + '/pgbouncer.ini', pgbouncerIni),
-			writeFile(LINUX_DEFAULT_CONFIG_BASE_DIR + '/userlist.txt', userlistTxt),
+			writeFileAndLog(LINUX_DEFAULT_CONFIG_BASE_DIR + '/pgbouncer.ini', pgbouncerIni),
+			writeFileAndLog(LINUX_DEFAULT_CONFIG_BASE_DIR + '/userlist.txt', userlistTxt),
 		])
 	}
 
