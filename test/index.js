d4bbda84a677f9fc966272f840f5c8619bd3c551
Jannis R <mail@jannisr.de>
2023-10-23 00:21:07 +0100

    refactor lib/map.js ✅

--- test/index.js
+++ test/index.js
@@ -1,7 +1,53 @@
 import test from 'node:test'
+import {
+	deepStrictEqual,
+	strictEqual,
+} from 'node:assert'
+import { inspect } from 'node:util' // todo: remove
+import {
+	parseEtcdEntries,
+	mapEtcdEntriesToPgbouncerIni,
+	mapEtcdEntriesToUserlistTxt,
+} from '../lib/map.js'
 
-import {todo} from '../index.js'
+const ETCD_STATE_1 = Object.freeze({
+	'foo': 'bar',
+	'users.alice': 'pool_mode=statement',
+	'users.bob': 'pool_mode=transaction',
+	'databases.db1': 'host=db1 user=hello password=world',
+	'userlist.alice': 'password',
+	'userlist.bob': 'some"password',
+})
 
-test('todo', (t) => {
-	// todo
+test('parseEtcdEntries works', (t) => {
+	const data = parseEtcdEntries(ETCD_STATE_1)
+	console.error('data', inspect(data, {depth: null}))
+
+	deepStrictEqual(data, {
+		'pgbouncer.ini': {
+			pgbouncer: {
+				foo: 'bar',
+			},
+			users: {
+				alice: 'pool_mode=statement',
+				bob: 'pool_mode=transaction',
+			},
+			databases: {
+				db1: 'host=db1 user=hello password=world',
+			},
+		},
+		'userlist.txt': {
+			alice: 'password',
+			bob: 'some"password',
+		},
+	})
+})
+
+test('mapEtcdEntriesToUserlistTxt works', (t) => {
+	const userlist = mapEtcdEntriesToUserlistTxt(ETCD_STATE_1)
+
+	strictEqual(userlist, `\
+"alice" "password"
+"bob" "some"password"
+`)
 })
